6b63717aed7ab02dde522a64116ee330ddbffc09
Phat Duong <duongp@wit.edu>
2019-11-09 17:08:23 -0500

    sceneOutline

--- WIT Hackathon 2019 Fall Project/src/sceneOutline.java
+++ WIT Hackathon 2019 Fall Project/src/sceneOutline.java
@@ -1,36 +1,60 @@
 import java.io.File;
+import java.io.IOException;
 import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
 
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
+
 public class sceneOutline {
-    private String background;
-    private String dialogue;
-    private String character;
-    private String music;
+    private File background;
+    private File dialogue;
+    private File character;
+    private Media music;
 
     public sceneOutline(){
+        //default constructor
     }
 
-    public void setBackground(String file){
-       this.background = "\\Images\\Background\\"+file;
+    public sceneOutline(String background, String dialogue, String character, String music) {
+        setBackground(background);
+        setDialogue(dialogue);
+        setCharacter(character);
+        setMusic(music);
     }
 
-    public void setCharacter(String file){
-        this.background = "\\Images\\Character\\"+file;
+    public void setBackground(String file) {
+        this.background = new File("\\Images\\Background\\" + file);
     }
 
-    //setDialougue("python.txt")
-    public void setDialogue(String file){
-        this.dialogue = "\\text\\"+file;
+    public void setCharacter(String file) {
+        this.character = new File("\\Images\\Character\\" + file);
     }
 
-    public BufferedImage getBackground(){
-        return ImageIO.read(new File(this.background));
+    // setDialougue("python.txt")
+    public void setDialogue(String file) {
+        this.dialogue = new File("\\text\\" + file);
     }
 
-    public File getCharacter(){
-        return new File(this.background);
+    public void setMusic(String file){
+        this.music = new Media("\\music\\" + file);
     }
 
+    public BufferedImage getBackground() throws IOException{
+        return ImageIO.read(this.background);
+    }
+
+    public BufferedImage getCharacter() throws IOException{
+        return ImageIO.read(this.character);
+    }
+
+    public File getDialogue() throws IOException{
+        return dialogue;
+    }
+
+    public MediaPlayer getMusic(){
+        return new MediaPlayer(this.music);
+    }
 
 }
