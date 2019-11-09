f745a4bcdc755aac9b4b6515b2219e5793999954
Phat Duong <duongp@wit.edu>
2019-11-09 16:51:53 -0500

    Image File created
    do not use sceneoutline

--- WIT Hackathon 2019 Fall Project/src/sceneOutline.java
+++ WIT Hackathon 2019 Fall Project/src/sceneOutline.java
@@ -1,4 +1,36 @@
+import java.io.File;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
 
 public class sceneOutline {
+    private String background;
+    private String dialogue;
+    private String character;
+    private String music;
+
+    public sceneOutline(){
+    }
+
+    public void setBackground(String file){
+       this.background = "\\Images\\Background\\"+file;
+    }
+
+    public void setCharacter(String file){
+        this.background = "\\Images\\Character\\"+file;
+    }
+
+    //setDialougue("python.txt")
+    public void setDialogue(String file){
+        this.dialogue = "\\text\\"+file;
+    }
+
+    public BufferedImage getBackground(){
+        return ImageIO.read(new File(this.background));
+    }
+
+    public File getCharacter(){
+        return new File(this.background);
+    }
+
 
 }
